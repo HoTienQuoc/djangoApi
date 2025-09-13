551131371ffb3f4ed52f032837a6c089cd935c9e
HoTienQuoc <hotienquoc0429@gmail.com>
2025-09-13 22:12:17 +0700

    Update Django settings to use local configuration and modify admin URL path

--- django-bank/api/config/urls.py
+++ django-bank/api/config/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin  # pyright: ignore[reportMissingImports]
+from django.conf import settings
+from django.contrib import admin
 from django.urls import path  # pyright: ignore[reportMissingImports]
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
 ]
